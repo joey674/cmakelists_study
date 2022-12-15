40883f4743051bc5bcf0654925c7b66141ef0d7c
joey674 <95547613+joey674@users.noreply.github.com>
2022-12-15 16:02:16 +0100

    Add files via upload

---
+++ hello_singlefile/build/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/guanzhouyi/Desktop/cmake_test/hello/source/hello.cpp" "CMakeFiles/hello.dir/source/hello.o" "gcc" "CMakeFiles/hello.dir/source/hello.o.d"
+  "/Users/guanzhouyi/Desktop/cmake_test/hello/source/hello1.cpp" "CMakeFiles/hello.dir/source/hello1.o" "gcc" "CMakeFiles/hello.dir/source/hello1.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hello_singlefile/source/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"hello"<<std::endl;
+    std::cout<<"hello is successfully built"<<std::endl;
+    return 0;
+}
